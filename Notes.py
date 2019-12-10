076e03742b8e661ac50f44b1ac7700cd429d24cd
Jay <jayi2@icloud.com>
2019-12-10 11:31:18 -0800

    Notes and Such

---
+++ Notes.py
@@ -0,0 +1,18 @@
+#!/usr/bash/python3
+
+@app.route('/')
+# @app.route: A way to teach python what to do when an asynchronous even occurs.
+# Means if someone sends sends an HTTP command to the code in the parenthesis, run the code underneath
+
+if __name__ == '__main__':
+
+
+
+# Ansible: State management
+# 'State' : Active or inactive
+
+#DevOps: A process of automating software and operations flow for decreased time and increased efficiency
+# Only use DevOps to automate a process you already know how to do yourself manually. DevOps is like a calculator for
+# simple math. You're already aware of processes that are taking place underneath, only now you don't have to take the
+# time to actually do it. Use DevOps to solve a problem you already have.
+# Response: Number code sent back
